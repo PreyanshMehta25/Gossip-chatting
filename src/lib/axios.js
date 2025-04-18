ebaa16d42561a255c00370add4f9bfe00da371aa
PreyanshMehta25 <148552801+PreyanshMehta25@users.noreply.github.com>
2025-04-18 18:56:52 +0530

    Delete src directory

--- src/lib/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api",
-    withCredentials: true,
-});
-
-export default axiosInstance;
--- src/lib/utils.js
+++
@@ -1,7 +0,0 @@
-export function formatMessageTime(date) {
-    return new Date(date).toLocaleDateString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: false,
-    });
-}
